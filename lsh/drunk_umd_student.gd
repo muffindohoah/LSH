adef56c1ef43ca89733a395dc752de347e6a0a11
muffindoh <uwufunkytown@gmail.com>
2025-09-10 21:39:10 -0500

    test room. player heavily fixed

--- lsh/drunk_umd_student.gd
+++ lsh/drunk_umd_student.gd
@@ -1,24 +1,25 @@
 extends CharacterBody2D
 
-const speed = 300.0
+const speed = 150.0
 var direction = Vector2(0,0)
 
 func _physics_process(delta):
-	movement(delta)
+	movement()
 
-func movement(d):
-	if Input.is_action_just_pressed("right"):
-		direction.x = 1
-	if Input.is_action_just_pressed("left"):
-		direction.x = -1
-	if Input.is_action_just_pressed("up"):
-		direction.y = -1
-	if Input.is_action_just_pressed("down"):
-		direction.y = 1
+func movement():
+	var input_dir:Vector2 = Vector2(0,0)
+	if Input.is_action_pressed("right"):
+		input_dir.x = 1
+	if Input.is_action_pressed("left"):
+		input_dir.x = -1
+	if Input.is_action_pressed("up"):
+		input_dir.y = -1
+	if Input.is_action_pressed("down"):
+		input_dir.y = 1
 
-	if Input.is_action_just_pressed("right") and Input.is_action_just_pressed("left"):
-		direction.x = 0
-	if Input.is_action_just_pressed("up") and Input.is_action_just_pressed("down"):
-		direction.y = 0
-		
-	move_and_collide(direction.normalized() * speed * d)
+	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
+		input_dir.x = 0
+	if Input.is_action_pressed("up") and Input.is_action_pressed("down"):
+		input_dir.y = 0
+	velocity = input_dir.normalized() * speed
+	move_and_slide()
