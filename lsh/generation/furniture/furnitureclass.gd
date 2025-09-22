c69096f92d0f58510072d4a881366279449ffa1a
muffindoh <uwufunkytown@gmail.com>
2025-09-22 18:08:20 -0500

    Update furnitureclass.gd

--- lsh/generation/furniture/furnitureclass.gd
+++ lsh/generation/furniture/furnitureclass.gd
@@ -7,7 +7,9 @@ class_name Furniture
 @export var collision_vector: Vector2
 @export var openable: bool
 @export var opened_texture: Texture
+@export var item_drop_pos:Vector2 = Vector2(0,0)
 @export var emits_light: bool
 @export var light_size: float = 0.5
 
 # TODO: Finish dev and remove the exports
+# the exports make no compromise nimwad. these are for creating resources in editor.
